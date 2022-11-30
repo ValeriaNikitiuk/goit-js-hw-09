f9206eabe4061dc28e068ce117c52f971755a0b3
ValeriaOvod <krossovki.ukraine.vzutya@gmail.com>
2022-11-30 20:01:58 +0200

    ready homework 9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,24 +2,31 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const form = document.querySelector('.form');
 form.addEventListener('submit', onSubmitForm);
 
- 
+
 
 function onSubmitForm (eve) {
   eve.preventDefault()
-  const amountValue = Number(eve.currentTarget.amount.value);
+  const amountValue = parseInt(eve.currentTarget.amount.value);
+  const stepVal = parseInt(eve.currentTarget.step.value);
+let delayVal = parseInt(eve.currentTarget.delay.value);
+ 
 
-  for (let position = 0; position <= amountValue; position += 1){
+  for (let position = 1; position <= amountValue; position += 1){
+    let delay = delayVal += position === 1 ? 0 : stepVal;
+  createPromise({ position, delay })
 
-createPromise(2, 1500)
   .then(({ position, delay }) => {
     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
+  },)
   .catch(({ position, delay }) => {
    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });}}
+  },)
+  }  
+} 
 
 
 function createPromise({ position, delay }) {
+  
   const shouldResolve = Math.random() > 0.3;
  return new Promise((resolve, reject) => {
     setTimeout(() => {
